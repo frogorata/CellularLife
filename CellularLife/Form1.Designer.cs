353cf9b96d245e9f3360814b5177016d8438ec90
frogorata <157526365+frogorata@users.noreply.github.com>
2026-01-08 12:20:45 +0100

    Добавьте файлы проекта.

---
+++ CellularLife/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace CellularLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 461);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CellularLife/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CellularLife
+{
+    public partial class Form1 : Form
+    {
+        private readonly Timer _timer = new Timer();
+        private const int gap = 30;
+        private const int row = 14;
+        private const int col = 31;
+        public bool[,] matrixCellIsLive = new bool[row, col];
+
+        private class Cell
+        {
+            Random random = new Random();
+            public bool isLive = false;
+            Rectangle rectangle = new Rectangle();
+            private int cellSize = 25;
+
+            public bool ovarianCell(int x, int y)
+            {
+                return true;
+            }
+            public Rectangle GetBounds(int StepX, int StepY, int pictureBox1Width, int pictureBox1Height)
+            {
+                rectangle.X += StepX;
+                rectangle.Y += StepY;
+                rectangle.Width = cellSize;
+                rectangle.Height = cellSize;
+                return rectangle;
+            }
+
+            public bool GetRandomBool()
+            {
+                bool value = random.Next(2) == 0;
+                return value;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer.Interval = 500;
+            _timer.Tick += TimerTick;
+            _timer.Start();
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            GenerationCells();
+            RenderingCell();
+        }
+        private void GenerationCells()
+        {
+
+        }
+        private void RenderingCell()
+        {
+            Random random = new Random();
+            Cell cell = new Cell();
+            Rectangle rect = cell.GetBounds(25, 25, pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+
+            Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            pictureBox1.Image = bmp;
+            Graphics g = Graphics.FromImage(bmp);
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrixCellIsLive[i, j] = cell.GetRandomBool();
+                    if (matrixCellIsLive[i, j] == cell.isLive)
+                        g.FillRectangle(Brushes.Black, rect.X, rect.Y, rect.Width, rect.Height);
+                    else
+                        g.FillRectangle(Brushes.White, rect.X, rect.Y, rect.Width, rect.Height);
+                    rect = cell.GetBounds(gap, 0, pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                }
+                rect = cell.GetBounds(-gap * col, gap, pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
